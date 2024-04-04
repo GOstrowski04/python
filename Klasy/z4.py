6714a378785a5a4db2ace3fb790dabacfedc9c3e
GOstrowski04 <148046211+GOstrowski04@users.noreply.github.com>
2024-04-04 15:27:48 +0200

    Update z4.py

--- Klasy/z4.py
+++ Klasy/z4.py
@@ -7,30 +7,39 @@ def zamiana_na_rzymskie(x):
               5: 'V', 4: 'IV', 1: 'I'}
     for i in liczby:
         if x / i >= 1.0:
-            y = int(x/i)
+            y = int(x / i)
             for j in range(0, y):
                 s += liczby[i]
                 x -= i
     return s
 
+
+def zamiana_na_arabskie(z):
+    wartosci = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    s = z[::-1]
+    liczba = 0
+    temp1 = 0
+    for i in s:
+        temp2 = wartosci[i]
+        if temp2 < temp1:
+            liczba -= temp2
+        else:
+            liczba += temp2
+        temp1 = temp2
+    return liczba
+
+
 class Romanian:
     def __init__(self, rzymska):
         self.rzymska = rzymska
 
-    def zamiana_na_arabskie(self):
-        wartosci = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        s = self.rzymska[::-1]
-        liczba = 0
-        temp1 = 0
-        for i in s:
-            temp2 = wartosci[i]
-            if temp2 < temp1:
-                liczba -= temp2
-            else:
-                liczba += temp2
-            temp1 = temp2
-        return liczba
+    def dodawanie(self, other):
+        x = zamiana_na_arabskie(self.rzymska)
+        y = zamiana_na_arabskie(other.rzymska)
+        return zamiana_na_rzymskie(x + y)
+
 
 r1 = Romanian('VII')
-print(r1.zamiana_na_arabskie())
+r2 = Romanian('CXVII')
 print(zamiana_na_rzymskie(1000))
+print(r1.dodawanie(r2))
